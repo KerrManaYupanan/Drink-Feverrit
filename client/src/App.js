ce82b33e6fb8cba2622190b39479b6e04a5b6528
ManaYupanan <62202448+ManaYupanan@users.noreply.github.com>
2021-03-18 10:55:08 +0700

    Update have a insert users

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
 import Axios from 'axios';
-import { useState, usestate } from 'react';
+import { useState } from 'react';
 function App() {
-  const [userList, setUserList] = useState([])
+  const [userList, setUserList] = useState([]);
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [position, setPosition] = useState("");
+  const [feverritDrink, setFeverritDrink] = useState("");
 
   const getUser = () => {
     Axios.get('http://localhost:3001/users').then((res) => {
       setUserList(res.data);
     });
+  }
+
+  const insertFeverritDrink = () => {
+    Axios.post('http://localhost:3001/create', {
+      name: name,
+      age: age,
+      position: position,
+      feverritDrink: feverritDrink
+    }).then(() => {
+      setUserList([
+        ...userList,
+        {
+          name: name,
+          age: age,
+          position: position,
+          feverritDrink: feverritDrink
+        },
+      ])
+    })
   }
 
   return (
@@ -18,27 +42,39 @@ function App() {
           <form action="">
             <div className="mb-3">
               <label className="form-label" htmlFor="name">Name : </label>
-              <input className="form-control" type="text" placeholder="Enter Name" />
+              <input className="form-control" type="text" placeholder="Enter Name"
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+              />
             </div>
             <div className="mb-3">
               <label className="form-label" htmlFor="age">Age : </label>
-              <input className="form-control" type="number" placeholder="Enter Age" />
+              <input className="form-control" type="number" placeholder="Enter Age"
+                onChange={(event) => {
+                  setAge(event.target.value);
+                }}
+              />
             </div>
             <div className="mb-3">
               <label className="form-label" htmlFor="position">Position : </label>
-              <input className="form-control" type="text" placeholder="Enter Position" />
+              <input className="form-control" type="text" placeholder="Enter Position"
+                onChange={(event) => { setPosition(event.target.value) }}
+              />
             </div>
             <div className="mb-3">
-              <label className="form-label" htmlFor="feverrit-drink">Feverrit Drink : </label>
-              <input className="form-control" type="text" placeholder="Enter Feverrit Drink" />
+              <label className="form-label" htmlFor="feverritdrink">Feverrit Drink : </label>
+              <input className="form-control" type="text" placeholder="Enter Feverrit Drink"
+                onChange={(event) => { setFeverritDrink(event.target.value) }}
+              />
             </div>
-            <button className="btn btn-success form-control mb-3">Add Feverrit Drink</button>
+            <button className="btn btn-success form-control mb-3" onClick={insertFeverritDrink}>Add Feverrit Drink</button>
           </form>
         </div>
         <hr />
         <div className="feverritdrink">
           <button className="btn btn-primary form-control" onClick={getUser}>Show Feverrit Drink</button>
-          <br/><br/>
+          <br /><br />
           {userList.map((val, key) => {
             return (
               <div className="users card  mb-2">
@@ -61,6 +97,7 @@ function App() {
           })}
 
         </div>
+        <br/>
       </div>
 
     </div>
--- server/index.js
+++ server/index.js
@@ -23,10 +23,28 @@ app.get('/users',(req,res)=>{
     })
 })
 
+app.post('/create' , (req ,res)=>{
+    const name = req.body.name;
+    const age = req.body.age;
+    const position = req.body.position;
+    const feverritDrink = req.body.feverritDrink;
+    db.query("INSERT INTO users (name,age,position,feverritDrink) VALUES (?,?,?,?)" , [name,age,position,feverritDrink],
+        (err ,res) =>{
+            if (err){
+                console.log(err)
+            } else {
+                alert('Insert is success')
+                res.send("Values Inserted")
+                console.log('Values insert is success')
+            }
+        }
+    )
+});
+
 app.listen('3001' , (err)=>{
     if(err){
         console.log("Port error is "+ err)
     } else{
-        console.log("Server runing on port 3000")
+        console.log("Server runing on port 3001")
     }
 })
